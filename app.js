c9dde108a23718cdc19162aea541bd76d092e739
sysh333 <sysh324@gmail.com>
2018-05-03 22:01:13 +0900

    add skeleton for viewmanagement

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const add = require('./adder.js');
-console.log(add(5, 6));
+
+class ViewManager {
+
+	conectEventHandlers() {
+		//wire up event handler for form submit
+	}
+
+	onSubmit(event) {
+
+		// block form from actually submitting
+
+		//grad the number values as strings
+
+		//cast the strings to ints
+
+		// add the numbers
+
+		//output
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.conectEventHandlers();
