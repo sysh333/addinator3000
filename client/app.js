89a5d6a477e08beefa76f9a4f82dc0be89020340
sysh333 <sysh324@gmail.com>
2018-05-05 14:30:25 +0900

    3-24 finishi

--- client/app.js
+++ client/app.js
@@ -4,19 +4,28 @@ class ViewManager {
 
 	conectEventHandlers() {
 		//wire up event handler for form submit
+		document.getElementById('form-numbers')
+			.addEventListener('submit', this.onSubmit);
 	}
 
 	onSubmit(event) {
 
 		// block form from actually submitting
+		event.preventDefault();
 
 		//grad the number values as strings
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
 
 		//cast the strings to ints
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
 
 		// add the numbers
+		const sum = add(num1, num2);
 
 		//output
+		alert(sum);
 	}
 }
 
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const url  = require('url');
+
+const server = http.createServer(function(request, response){
+	const urlPath = url.parse(request.url).pathname;
+	let filePath = `./client/${urlPath}`
+
+	fs.stat(filePath, function(err, fileInfo){
+		if (!err && fileInfo.isDirectory()) {
+			filePath += '/index.html';
+		}
+
+		fs.exists(filePath, function(doesExist){
+			if (!doesExist) {
+				response.statusCode = 404;
+				response.end(`Resource not found: "${urlPath}"`);
+			} else {
+				fs.readFile(filePath, (err, data) =>{
+					if (err) {
+						response.statusCode = 500;
+						response.end(`Server error: "${err}"`);
+					} else {
+						response.end(data.toString('UTF-8'));
+					}
+				});
+			}
+
+		});
+	});
+
+
+});
+
+server.listen(3000, function() {
+	console.log('Server listening ...');
+});
