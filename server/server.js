eb493059858a964292c2c5fb570dad1d1272cb95
sysh333 <sysh324@gmail.com>
2018-05-06 22:59:32 +0900

    add browserify dependency and PORT dependency

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ const server = http.createServer(function(request, response){
 
 });
 
-server.listen(3000, function() {
-	console.log('Server listening ....');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
+	console.log(`Server listening on port ${PORT}...`);
 });
